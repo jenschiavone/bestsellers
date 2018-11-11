82b3f42d91ef023e9c6cc7b0333426333f469676
Jennifer Schiavone <jennifer.schiavone@manheim.com>
2018-11-11 08:38:10 -0500

    Update app name in server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static('./dist/jls-bestsellers'));
+app.use(express.static('./dist/bestsellers'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/jls-bestsellers/index.html'));
+res.sendFile(path.join(__dirname,'/dist/bestsellers/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
